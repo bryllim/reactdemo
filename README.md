584f4786a7a1cc83d53bce057c8691c284190cff
bryllim <bryllim@gmail.com>
2023-10-17 16:36:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+1. React Components
+2. States
+3. Props
---
+++ src/Todo.jsx
@@ -0,0 +1,7 @@
+function Todo({todo, index}){
+    return (
+        <div className="alert bg-light my-2" index={index}>{todo}</div>
+    )
+}
+
+export default Todo;
