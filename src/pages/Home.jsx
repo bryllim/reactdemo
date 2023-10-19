61f1898e4dc8a1e7a82caae3a7dc1dcd04f53239
bryllim <bryllim@gmail.com>
2023-10-19 15:59:33 +0800

    added react router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <section>
+            <h1>This is the home page!</h1>
+            <p>I am an example introduction to this website.</p>
+        </section>
+    )
+}
+
+export default Home;
